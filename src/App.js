f4942fa151a4a5fac0ffeedc5b00af14dfb1ce9f
haider <rabeeahhaydeer@gmail.com>
2022-09-02 09:36:17 +0300

    Hero Banner .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import "./App.css";
+import { Route,Routes } from 'react-router-dom';
+import {Box} from "@mui/material";
+import ExerciseDetails from './pages/ExerciseDetails';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400" sx={{width:{x1:'1499px'}}} m="auto">
+     
+        <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/exercise/:id" element={<ExerciseDetails/>}/>
+      </Routes>
+      <Footer/>
+      
+
+    </Box>
   )
 }
 
